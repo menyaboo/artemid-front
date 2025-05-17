aa077b064979c7630b203c6f49a5aad282b85be3
menyaboo <tosha.kolmay@gmail.com>
2025-05-17 21:51:26 +0700

    fix: select

--- src/shared/ui/popover/index.tsx
+++ src/shared/ui/popover/index.tsx
@@ -1,9 +1,10 @@
-import { FC, PropsWithChildren, ReactNode, useState } from 'react';
+import { FC, ReactNode, useEffect, useRef, useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 
-interface PopoverProps extends PropsWithChildren {
+interface PopoverProps {
   trigger: (isOpen: boolean) => ReactNode;
   placement?: keyof typeof positionClasses;
+  children?: (handleClose: () => void) => ReactNode;
 }
 
 const positionClasses = {
@@ -15,9 +16,31 @@ const positionClasses = {
 
 const Popover: FC<PopoverProps> = ({ trigger, children, placement = 'bottom' }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const popoverRef = useRef<HTMLDivElement>(null);
+
+  const handleClose = () => setIsOpen(false)
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        popoverRef.current &&
+        !popoverRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen]);
 
   return (
-    <div className="relative inline-block">
+    <div className="relative inline-block" ref={popoverRef}>
       <div onClick={ () => setIsOpen((prev) => !prev) } className="cursor-pointer">
         { trigger(isOpen) }
       </div>
@@ -31,7 +54,7 @@ const Popover: FC<PopoverProps> = ({ trigger, children, placement = 'bottom' }) 
             transition={ { type: 'spring', stiffness: 300, damping: 25 } }
             className={ `absolute z-50 w-full ${ positionClasses[placement] } bg-background-main border border-background-tertiary rounded shadow-md p-2` }
           >
-            { children }
+            { children?.(handleClose) }
           </motion.div>
         ) }
       </AnimatePresence>
--- src/shared/ui/select/base/index.tsx
+++ src/shared/ui/select/base/index.tsx
@@ -4,45 +4,56 @@ import { ICheckedStatedProps, ILabelValueProps } from "@shared/interface/ui";
 import { ChevronDown, ChevronUp } from "lucide-react";
 
 interface IBaseSelectProps<TValue, TItem extends ILabelValueProps<TValue>> extends ICheckedStatedProps<TValue, TItem> {
-  placeholder?: string;
-  error?: string;
+	placeholder?: string;
+	error?: string;
 }
 
 const Select = <TValue, TItem extends ILabelValueProps<TValue>>(
-  { items, value, onChange, error, placeholder = 'Выберите значение' }: IBaseSelectProps<TValue, TItem>
+	{ items, value, onChange, error, placeholder = 'Выберите значение' }: IBaseSelectProps<TValue, TItem>
 ): ReactNode => {
-  const selectedLabel = items.find((opt) => opt.value === value)?.label || placeholder;
+	const selectedLabel = items.find((opt) => opt.value === value)?.label || placeholder;
 
-  return (
-    <Popover
-      trigger={ (isOpen) =>
-        <div>
-          <div
-            className="border border-background-tertiary rounded px-4 py-2 min-w-[200px] gap-4 flex justify-between items-center bg-background-main cursor-pointer">
-            <span>{ selectedLabel }</span>
-            <span className="text-gray-500 transform transition-transform">{ isOpen ? <ChevronUp/> :
-              <ChevronDown/> }</span>
-          </div>
-          {error && <span className="text-error">{error}</span>}
-        </div>
-      }
-      placement="bottom"
-    >
-      <div className="flex flex-col max-h-60 overflow-auto">
-        { items.map((option) => (
-          <div
-            key={ option.label }
-            onClick={ () => onChange?.(option.value) }
-            className="px-4 py-2 hover:bg-background-secondary transition-colors duration-300 ease-in-out rounded-md cursor-pointer"
-          >
-            { option.label }
-          </div>
-        )) }
-      </div>
-    </Popover>
-  );
+	const Children = (handleClose: () => void) => (
+		<div className="flex flex-col max-h-60 overflow-auto">
+			{ items.map((option) => {
+				const handleClick = () => {
+					onChange?.(option.value)
+					handleClose()
+				}
+				return (
+					<div
+						key={ option.label }
+						onClick={ handleClick }
+						className="px-4 py-2 hover:bg-background-secondary transition-colors duration-300 ease-in-out rounded-md cursor-pointer"
+					>
+						{ option.label }
+					</div>
+				)
+			}) }
+		</div>
+	)
+
+
+	return (
+		<Popover
+			trigger={ (isOpen) =>
+				<div>
+					<div
+						className="border border-background-tertiary rounded px-4 py-2 min-w-[200px] gap-4 flex justify-between items-center bg-background-main cursor-pointer">
+						<span>{ selectedLabel }</span>
+						<span className="text-gray-500 transform transition-transform">{ isOpen ? <ChevronUp/> :
+							<ChevronDown/> }</span>
+					</div>
+					{ error && <span className="text-error">{ error }</span> }
+				</div>
+			}
+			placement="bottom"
+		>
+			{ Children }
+		</Popover>
+	);
 };
 
 export {
-  Select
+	Select
 }
